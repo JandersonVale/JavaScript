183c32a2283f40aae9cbee49109a239eeb499885
jbzr developer <65793382+JandersonVale@users.noreply.github.com>
2024-10-27 19:41:39 -0300

    Hello World! - Olá Mundo!
    Código simples usando  linguagem JavaScript, solicitando dados do usuário em uma constante, e exibindo uma mensagem

---
+++ Hello World.js
@@ -0,0 +1,5 @@
+// solicita o nome do usuário
+const nome = prompt("Por favor, digite seu nome:");
+
+// Exibe a mensagem personalizada
+alert(`Hello World!, eu sou o ${nome}, sejam bem vindos ao mundo da programação!`);
